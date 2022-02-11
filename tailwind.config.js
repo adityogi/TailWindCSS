75b94ea3c84d93865fab339dc63cf7d7fbe7c952
adityogi <73770862+adityogi@users.noreply.github.com>
2022-02-11 13:06:00 +0530

    Add Files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: {
+    content: ["./index.html/"],
+    enabled: true
+  },
+  content: [
+    './index.html/'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
